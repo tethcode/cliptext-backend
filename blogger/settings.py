4ac8801738a44e690190f1202d52455dc52008dd
Emmanuel Yakubu <tethcode@gmail.com>
2026-03-14 16:08:51 +0100

    Update email backend to use SSL instead of TLS
    Changed email backend settings from TLS to SSL.

--- blogger/settings.py
+++ blogger/settings.py
@@ -161,8 +161,9 @@ CLOUDINARY_STORAGE = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True   # Changed from TLS to SSL
+EMAIL_USE_TLS = Fals
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
