163717b122b567abb04a80db8508c7b9c0a6bff6
Emmanuel Yakubu <tethcode@gmail.com>
2026-03-14 15:51:46 +0100

    Restore Django settings with updated configurations

--- blogger/settings.py
+++ blogger/settings.py
@@ -146,7 +146,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE': True
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
