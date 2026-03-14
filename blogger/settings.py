2d5f1f4f2c01f8732aaae7690b2f256bf3135f0d
Emmanuel Yakubu <tethcode@gmail.com>
2026-03-14 16:25:52 +0100

    Restore Django settings with necessary configurations

--- blogger/settings.py
+++ blogger/settings.py
@@ -163,7 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True   # Changed from TLS to SSL
-EMAIL_USE_TLS = Fals
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
