d3f4b4d6a9542c66a3ab441ed32b391d8615108f
Emmanuel Yakubu <tethcode@gmail.com>
2026-03-14 16:35:02 +0100

    Restore Django settings with updated configurations

--- blogger/settings.py
+++ blogger/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
